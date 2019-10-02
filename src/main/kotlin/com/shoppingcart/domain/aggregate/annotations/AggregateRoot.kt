ec32b84fac78ff9c685b46943629315023e5c7a6
medineceylan <medine.gulal@gmail.com>
2019-10-02 16:58:07 +0100

    Remove retention from annotations because kotlin support runtime retension as default

--- src/main/kotlin/com/shoppingcart/domain/aggregate/annotations/AggregateRoot.kt
+++ src/main/kotlin/com/shoppingcart/domain/aggregate/annotations/AggregateRoot.kt
@@ -1,5 +1,4 @@
 package com.shoppingcart.domain.aggregate.annotations
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
 annotation class AggregateRoot
--- src/main/kotlin/com/shoppingcart/domain/aggregate/annotations/Entity.kt
+++ src/main/kotlin/com/shoppingcart/domain/aggregate/annotations/Entity.kt
@@ -2,7 +2,6 @@ package com.shoppingcart.domain.aggregate.annotations
 
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
 annotation class Entity
 
 
